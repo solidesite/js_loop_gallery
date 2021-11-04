8a7212dd326e8bf586187f078fbc9a452116ff17
solidesite <solidesite@gmail.com>
2021-11-04 20:29:26 +0900

    left, right 버튼 기능 구현

--- js/control.js
+++ js/control.js
@@ -1,15 +1,20 @@
 const box = document.querySelector('.box').cloneNode(true);
 const gall = document.querySelector('.gall');
+const leftBtn = document.querySelector('.left');
+const rightBtn = document.querySelector('.right');
 
 gall.append(box)
 
 let num = 0;
+let speed = 1;
 
 function motion() {
-  num -= 2;
+  num -= 2*speed;
   gall.style.left = num + 'px';
   if (num < -1200) {
     num = 0;
+  } else if (num > 0) {
+    num = -1200;
   }
 }
 
@@ -17,3 +22,7 @@ function play() {
   setInterval(motion, 10);
 }
 play();
+
+leftBtn.addEventListener('click', ()=>{
+  speed *= -1;
+})
